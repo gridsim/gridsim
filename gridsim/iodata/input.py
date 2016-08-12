cce82e05760db306b951e586bfd252fa06379705
Gillian Basso <gillian.basso@hevs.ch>
2016-08-12 10:35:50 +0200

    Add CSVTimeReader for semsim

--- gridsim/iodata/input.py
+++ gridsim/iodata/input.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import warnings
 
@@ -22,9 +23,9 @@ class Reader(object):
         """
         raise NotImplementedError('Pure abstract method.')
 
-    @accepts((1, str))
+    @accepts((1, str), (2, int, float))
     @returns(dict)
-    def load(self, data_type=None):
+    def load(self, data_type=None, nb_data=float("inf")):
         """
         load(self, data_type=None)
 
@@ -39,6 +40,8 @@ class Reader(object):
         :param data_type: the type of stored data if ``None`` no conversion
             are done.
         :type data_type: type (default None)
+        :param nb_data: the number of data to load
+        :type nb_data: int
 
         :return: a dict of values
         :rtype: dict
@@ -89,9 +92,9 @@ class CSVReader(Reader):
         """
         self._data.clear()
 
-    @accepts((1, str))
+    @accepts((1, str), (2, int))
     @returns(dict)
-    def load(self, data_type=None):
+    def load(self, data_type=None, nb_data=sys.maxint):
         """
         load(self, data_type=None)
 
@@ -100,6 +103,8 @@ class CSVReader(Reader):
         :param data_type: the type of the stored data if ``None`` no conversion
             are done.
         :type data_type: type
+        :param nb_data: the number of line to load
+        :type nb_data: int
         """
 
         # Open the csv data file.
@@ -140,6 +145,79 @@ class CSVReader(Reader):
             for row in reader:
                 if len(row) is not len(data_names):
                     raise SyntaxError('Invalid gridsim CSV file.')
+
+                if len(self._data) >= nb_data:
+                    break
+
+                for i in range(0, len(row)):
+                    if data_type is None:
+                        self._data[data_names[i]].append(row[i])
+                    else:
+                        self._data[data_names[i]].append(data_type(row[i]))
+
+        return self._data
+
+
+class CSVTimeReader(CSVReader):
+
+    @accepts((1, str), (2, int, float))
+    @returns(dict)
+    def load(self, data_type=None, nb_data=float("inf")):
+        """
+        load(self, data_type=None)
+
+        Loads the data from the given CSV data file.
+
+        :param data_type: the type of the stored data if ``None`` no conversion
+            are done.
+        :type data_type: type
+        :param nb_data: the time the load MUST stop
+        :type nb_data: int
+            .. warning:: The first column is consider as time column.
+            ``nb_data`` MUST have the same ``unit`` as the time in the stream
+        """
+
+        # Open the csv data file.
+        with open(self._stream, 'r') as csv_data:
+
+            # create a sniffer
+            sniffer = csv.Sniffer()
+
+            # verify header
+            has_header = sniffer.has_header(csv_data.read())
+            if not has_header:
+                warnings.warn("The CVS data %s has no header" % self._stream,
+                              category=SyntaxWarning)
+
+            # return to the begin
+            csv_data.seek(0)
+
+            # sniff the data to find a header
+            dialect = sniffer.sniff(csv_data.read())
+            # return to the begin
+            csv_data.seek(0)
+
+            # Create the CSV parser.
+            reader = csv.reader(csv_data, dialect)
+
+            # Read header
+
+            data_names = reader.next()
+            if not has_header:
+                for i in range(0, len(data_names)):
+                    data_names[i] = CSVReader.DEFAULT_DATA_NAME+str(i)
+                csv_data.seek(0)
+
+            # initialise the dict
+            self._data = dict(zip(data_names, [[] for _ in data_names]))
+
+            # Load data into the object.
+            for row in reader:
+                if len(row) is not len(data_names):
+                    raise SyntaxError('Invalid gridsim CSV file.')
+
+                if data_type(row[0]) > data_type(nb_data):
+                    break
 
                 for i in range(0, len(row)):
                     if data_type is None:
