1c349f19dc7596e0be3026717b3aeb148b16215c
U-HEVS\yann.maret <yann.maret@WE5999.hevs.ch>
2016-11-28 13:21:04 +0100

    refactor aggregator

--- gridsim/cyberphysical/element/aggregator.py
+++ gridsim/cyberphysical/element/aggregator.py
@@ -7,50 +7,62 @@
 
 from gridsim.cyberphysical.core import Aggregator
 
-class SumAggregator(Aggregator):
-    def __init__(self):
-        """
+from gridsim.decorators import accepts, returns
 
+
+class SumAggregator(Aggregator):
+    def __init__(self, limit_min, limit_max, default=0):
+        """
         __init__(self)
 
         Sum Aggregator class. This takes all the value and make a simple sum over them and return
         the result.
-
         """
-        super(SumAggregator, self).__init__()
+        super(SumAggregator, self).__init__(limit_min, limit_max, default)
 
-    def call(self,datalist):
+    def call(self, datas):
         """
+        call(self,datas)
 
-        call(self,datalist)
+        Aggregate the datas with a sum and return the result
 
-        Aggregate the datalist with a sum and return the result
-
-        :param datalist: list of data to aggregate, in this case it's a sum
+        :param datas: list of data to aggregate, in this case it's a sum
         :return: the sum of the given list value
         """
         res = 0
-        for u in datalist:
+        for u in datas:
             try:
                 res = res + int(u)
             except TypeError:
                 raise Exception('Cannot aggregate data - TypeError on int conversion')
+
+        if res < self.limit_min:
+            if self.default is not 0:
+                res = self.default
+            else:
+                res = self.limit_min
+        elif res > self.limit_max:
+            if self.default is not 0:
+                res = self.default
+            else:
+                res = self.limit_max
+
         return res
 
-class MeanAggregator(Aggregator):
-    def __init__(self):
-        """
 
-        __init__(self)
+class MeanAggregator(Aggregator):
+    def __init__(self, limit_min, limit_max, default=0):
+        """
+        __init__(self,limit_min,limit_max,default)
 
         Mean Aggregator class. This does the average value after calling the SumAggregator 'call' function
-
         """
-        super(MeanAggregator, self).__init__()
+        super(MeanAggregator, self).__init__(limit_min, limit_max, default)
 
-        self._sumaggregator = SumAggregator()
+        self._sum_aggregator = SumAggregator(limit_min, limit_max, default)
 
-    def call(self,datalist):
-        #calculate the average value of the datalist passed in parameter
-        return self._sumaggregator.call(datalist)/len(datalist)
-
+    @accepts((1, list))
+    @returns((int, float))
+    def call(self, datas):
+        # calculate the average value of the datalist passed in parameter
+        return self._sum_aggregator.call(datas) / len(datas)
