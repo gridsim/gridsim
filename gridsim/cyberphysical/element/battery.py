ac2788171f90dafa8baf12feb6fd818fffa059e8
U-HEVS\yann.maret <yann.maret@WE5999.hevs.ch>
2016-11-28 13:27:50 +0100

    refactor domestichotwater.py

--- gridsim/cyberphysical/element/battery.py
+++ gridsim/cyberphysical/element/battery.py
@@ -10,10 +10,15 @@ from gridsim.core import AbstractSimulationElement
 from gridsim.cyberphysical.external import Actor
 from gridsim.cyberphysical.simulation import CyberPhysicalModuleListener
 
-class Battery(Actor,AbstractSimulationElement,CyberPhysicalModuleListener):
-    def __init__(self, friendly_name, start_energy, maxenergy, power, readparamlist, writeparamlist):
-        """
+from gridsim.decorators import accepts, returns
+from gridsim.unit import units
 
+
+class Battery(Actor, AbstractSimulationElement, CyberPhysicalModuleListener):
+    @accepts((1, str))
+    @units.wraps(None, (None, None, units.watt_hour, units.watt_hour, units.watt, None, None))
+    def __init__(self, friendly_name, start_energy, max_energy, power, read_params, write_params):
+        """
         __init__(self,friendly_name,start_energy,maxenergy,power,readparamist,writeparamlist)
 
         This class simulate the behavior of a battery. The amount of energy is provided at the beginning.
@@ -26,32 +31,40 @@ class Battery(Actor,AbstractSimulationElement,CyberPhysicalModuleListener):
 
         :param friendly_name: friendly name for the abstractsimulationelement
         :param start_energy: amount of energy to start with
-        :param maxenergy: max energy of the battery, the battery will stop storing
+        :param max_energy: max energy of the battery, the battery will stop storing
         :param power: power rate for the storage and discharge
-        :param readparamlist: read parameter for the actor
-        :param writeparamlist: write parameter for the actor
+        :param read_params: read parameter for the actor
+        :param write_params: write parameter for the actor
         """
+        # HACK: when object is constructed with *args or **kwargs
+        if not isinstance(start_energy, (int, float)):
+            start_energy = units.value(units.to_si(start_energy))
+        if not isinstance(max_energy, (int, float)):
+            max_energy = units.value(units.to_si(max_energy))
+        if not isinstance(power, (int, float)):
+            power = units.value(units.to_si(power))
+
         Actor.__init__(self)
-        AbstractSimulationElement.__init__(self,friendly_name)
+        AbstractSimulationElement.__init__(self, friendly_name)
         CyberPhysicalModuleListener.__init__(self)
 
-        self.readparamtype = readparamlist
-        self.writeparamtype = writeparamlist
+        self.read_params = read_params
+        self.write_params = write_params
 
         self._store = True
-        self._overload = False
+        self._over_load = False
         self._empty = False
-        if start_energy < maxenergy:
+        if start_energy < max_energy:
             self._energy = start_energy
             self.energy = start_energy
         else:
-            self._energy = maxenergy
-            self.energy = maxenergy
+            self._energy = max_energy
+            self.energy = max_energy
 
-        self._maxenergy = maxenergy
+        self._max_energy = max_energy
         self._power = power
 
-        #the simulation runs with second, this is the conversion factor
+        # the simulation runs with second, this is the conversion factor
         self._energy_seconds_to_hours = 3600.0
 
     def init(self):
@@ -60,54 +73,59 @@ class Battery(Actor,AbstractSimulationElement,CyberPhysicalModuleListener):
     def reset(self):
         self.energy = self._energy
 
+    @accepts(((1, 2), (int, float)))
     def update(self, time, delta_time):
         self.energy = self._energy
 
+    @accepts(((1, 2), (int, float)))
     def calculate(self, time, delta_time):
-        #get the amount of energy save or feed
-        e = delta_time*self._power/self._energy_seconds_to_hours
+        # get the amount of energy save or feed
+        e = delta_time * self._power / self._energy_seconds_to_hours
         if self._store:
+            # add energy to the battery in storage mode
             self._energy += e
         else:
+            # get energy from the battery in unload mode
             self._energy -= e
 
-        print self._energy
-
-        self._overload = False
+        self._over_load = False
         self._empty = False
+        # upper limit of the battery
+        if self._energy > self._max_energy:
+            self._energy = self._max_energy
+            self._over_load = True
 
-        #limit at the border of the battery
-        if self._energy > self._maxenergy:
-            self._energy = self._maxenergy
-            self._overload = True
-
+        # down limit of the battery
         if self._energy < 0:
             self._energy = 0
             self._empty = True
 
-    def notifyReadParam(self, info, data):
+    @accepts((2, (int, float)))
+    def notify_read_param(self, read_param, data):
         pass
 
-    def getValue(self, info):
-        print info, self.writeparamtype
-        if info in self.writeparamtype:
-            #save energy
+    @returns((int, float))
+    def get_value(self, write_param):
+        if write_param in self.write_params:
+            # save energy
             if self._store:
-                if not self._overload:
-                    print self._energy, self._power
+                if not self._over_load:
+                    # in storage mode the power feed in the battery
                     return self._power
                 else:
                     self._store = False
+                    # the battery is full, so it doesn't load anymore
                     return 0
-            #give energy
+            # give energy
             elif not self._store:
                 if not self._empty:
-                    print self._energy, -self._power
+                    # in injection mode the power is taken from the battery
                     return -self._power
                 else:
                     self._store = True
+                    # the batter is empty, so it doesn't inject anymore
                     return 0
+        return 0
 
-    def cyberphysicalModuleEnd(self):
-
+    def cyberphysical_module_end(self):
         print 'end simulation from Battery'
