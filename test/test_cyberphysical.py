6210dc5f9e53042bd9fa2110a50eca3916f89176
U-HEVS\yann.maret <yann.maret@WE5999.hevs.ch>
2016-12-02 16:15:45 +0100

    add test cyberphysical on sum aggregator

--- test/test_cyberphysical.py
+++ test/test_cyberphysical.py
@@ -1,13 +1,30 @@
 import unittest
 
 
+from gridsim.cyberphysical.aggregator import SumAggregator
+
 class TestCyberPhysical(unittest.TestCase):
-    def test_simulation_timermanager(self):
-        pass
+    def test_sum_aggregator(self):
 
-    def test_pqcontroller(self):
-        pass
+        sum = SumAggregator(-1000,1000,0)
 
+        l = [10,20,30]
+        res = sum.call(l)
+        self.assertEqual(res, 60)
+
+        l = [-1000,-1000,-1000]
+        res = sum.call(l)
+        self.assertEqual(res, -1000)
+
+        l = [1000,1000,1000]
+        res = sum.call(l)
+        self.assertEqual(res, 1000)
+
+        sum = SumAggregator(-1000, 1000, 100)
+
+        l = [1000,1000,1000]
+        res = sum.call(l)
+        self.assertEqual(res, 100)
 
 if __name__ == '__main__':
     unittest.main()
